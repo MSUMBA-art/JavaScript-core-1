f24316d1a91163a919d277ed87837577277cd076
ALEXANDER <msumba83@gmail.com>
2024-03-23 07:55:54 +0200

    done numbers and Math method

--- js1-week1.js
+++ js1-week1.js
@@ -53,3 +53,43 @@ const helloName = `${sayHello} ${fullName}`;
 console.log(helloName);
 
 // =====Numbers======
+const sum = 10 + 2; // 12
+const product = 10 * 2; // 20
+const quotient = 10 / 2; // 5
+const difference = 10 - 2; // 8
+
+// Numbers with decimals can be rounded to the nearest whole number using the Math.round function: 
+const preciseAge = 30.487;
+const roughAge = Math.round(preciseAge)
+console.log(roughAge)
+
+// == EXERCISE =====
+// 1. Create two variables numberOfTrainees and numberOfMentors
+// 2. Log a message that displays the total number of trainees and mentors
+
+// Expected Result
+// Number of trainees: 15
+// Number of mentors: 8
+// Total number of trainees and mentors: 23
+const numberOfTrainees = 15;
+const numberOfMentors = 8;
+
+const total = numberOfTrainees + numberOfMentors;
+console.log(total)
+
+// ==== EXERCISE ===== 
+// Using the variables you created in the previous steps, calculate the percentage of mentors and trainees in the group (percentages must be rounded to the nearest integer)
+
+// You should then log this number to the console.
+
+// Expected Result
+
+// Percentage of trainees: 65%
+// Percentage of mentors: 35%
+
+let percentageOfTrainees = (numberOfTrainees / total) *  100
+console.log(Math.round(percentageOfTrainees));
+
+let percentageOfMentors = (numberOfMentors / total) * 100;
+console.log(Math.round(percentageOfMentors));
+
